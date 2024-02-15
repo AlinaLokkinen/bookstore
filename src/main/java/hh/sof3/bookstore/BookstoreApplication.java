351b5940020715bd2d26ec1891ea794b99965f84
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2024-02-15 12:13:51 +0200

    Category entity lisätty ja testattu

--- src/main/java/hh/sof3/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof3/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import hh.sof3.bookstore.domain.Book;
 import hh.sof3.bookstore.domain.BookRepository;
+import hh.sof3.bookstore.domain.Category;
+import hh.sof3.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -16,7 +18,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookRepository) {
+	public CommandLineRunner bookDemo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 		return (args) -> {
 			Book book1 = new Book("ekakirja", "eka kirjailija", 2000, "930505", 15.0);
 			Book book2 = new Book("tokakirja", "toka kirjailija", 2021, "929065", 15.2);
@@ -24,7 +26,15 @@ public class BookstoreApplication {
 			bookRepository.save(book1);
 			bookRepository.save(book2);
 
+			Category cat1 = new Category(1, "horror");
+			Category cat2 = new Category(2, "romance");
+
+			categoryRepository.save(cat1);
+			categoryRepository.save(cat2);
+
 		};
+
+	
 	}
 
 }
